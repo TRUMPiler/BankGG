ab1a639d3aaf14f090fec359471faffd1a05b2fe
TRUMPiler <101004472+TRUMPiler@users.noreply.github.com>
2023-06-04 02:04:22 +0530

    Part1
    Form is being animated done only main menu rest  is will be also animated soon

--- BankGG-windowsApp/BankGG/AddAccount.Designer.cs
+++ BankGG-windowsApp/BankGG/AddAccount.Designer.cs
@@ -29,6 +29,7 @@ namespace BankGG
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblName = new System.Windows.Forms.Label();
             this.FirstNtext = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -49,116 +50,130 @@ namespace BankGG
             this.SubmitBtn = new System.Windows.Forms.Button();
             this.Cancelbtn = new System.Windows.Forms.Button();
             this.Birthdate = new System.Windows.Forms.MonthCalendar();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
             this.SuspendLayout();
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(28, 29);
+            this.lblName.Location = new System.Drawing.Point(37, 36);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.Size = new System.Drawing.Size(49, 17);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Name:";
             // 
             // FirstNtext
             // 
             this.FirstNtext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.FirstNtext.Location = new System.Drawing.Point(89, 26);
+            this.FirstNtext.Location = new System.Drawing.Point(119, 32);
+            this.FirstNtext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.FirstNtext.Name = "FirstNtext";
-            this.FirstNtext.Size = new System.Drawing.Size(100, 20);
+            this.FirstNtext.Size = new System.Drawing.Size(132, 22);
             this.FirstNtext.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 47);
+            this.label2.Location = new System.Drawing.Point(37, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(109, 6);
+            this.label3.Location = new System.Drawing.Point(145, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.Size = new System.Drawing.Size(74, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "First name";
             // 
             // SecondNtext
             // 
             this.SecondNtext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.SecondNtext.Location = new System.Drawing.Point(212, 26);
+            this.SecondNtext.Location = new System.Drawing.Point(283, 32);
+            this.SecondNtext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SecondNtext.Name = "SecondNtext";
-            this.SecondNtext.Size = new System.Drawing.Size(100, 20);
+            this.SecondNtext.Size = new System.Drawing.Size(132, 22);
             this.SecondNtext.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(234, 6);
+            this.label4.Location = new System.Drawing.Point(312, 7);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.Size = new System.Drawing.Size(88, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Middle name";
             // 
             // ThirdNtext
             // 
             this.ThirdNtext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.ThirdNtext.Location = new System.Drawing.Point(328, 26);
+            this.ThirdNtext.Location = new System.Drawing.Point(437, 32);
+            this.ThirdNtext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ThirdNtext.Name = "ThirdNtext";
-            this.ThirdNtext.Size = new System.Drawing.Size(100, 20);
+            this.ThirdNtext.Size = new System.Drawing.Size(132, 22);
             this.ThirdNtext.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(343, 6);
+            this.label5.Location = new System.Drawing.Point(457, 7);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.Size = new System.Drawing.Size(74, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Last name";
             // 
             // Emaillbl
             // 
             this.Emaillbl.AutoSize = true;
-            this.Emaillbl.Location = new System.Drawing.Point(28, 60);
+            this.Emaillbl.Location = new System.Drawing.Point(37, 74);
+            this.Emaillbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Emaillbl.Name = "Emaillbl";
-            this.Emaillbl.Size = new System.Drawing.Size(35, 13);
+            this.Emaillbl.Size = new System.Drawing.Size(46, 17);
             this.Emaillbl.TabIndex = 8;
             this.Emaillbl.Text = "Email:";
             // 
             // Emailtext
             // 
             this.Emailtext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.Emailtext.Location = new System.Drawing.Point(89, 60);
+            this.Emailtext.Location = new System.Drawing.Point(119, 74);
+            this.Emailtext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Emailtext.Name = "Emailtext";
-            this.Emailtext.Size = new System.Drawing.Size(223, 20);
+            this.Emailtext.Size = new System.Drawing.Size(296, 22);
             this.Emailtext.TabIndex = 9;
             // 
             // Adresslbl
             // 
             this.Adresslbl.AutoSize = true;
-            this.Adresslbl.Location = new System.Drawing.Point(28, 96);
+            this.Adresslbl.Location = new System.Drawing.Point(37, 118);
+            this.Adresslbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Adresslbl.Name = "Adresslbl";
-            this.Adresslbl.Size = new System.Drawing.Size(48, 13);
+            this.Adresslbl.Size = new System.Drawing.Size(64, 17);
             this.Adresslbl.TabIndex = 10;
             this.Adresslbl.Text = "Address:";
             // 
             // Adresstext
             // 
-            this.Adresstext.Location = new System.Drawing.Point(89, 93);
+            this.Adresstext.Location = new System.Drawing.Point(119, 114);
+            this.Adresstext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Adresstext.Name = "Adresstext";
-            this.Adresstext.Size = new System.Drawing.Size(233, 96);
+            this.Adresstext.Size = new System.Drawing.Size(309, 117);
             this.Adresstext.TabIndex = 11;
             this.Adresstext.Text = "";
             // 
             // AccTypelbl
             // 
             this.AccTypelbl.AutoSize = true;
-            this.AccTypelbl.Location = new System.Drawing.Point(6, 201);
+            this.AccTypelbl.Location = new System.Drawing.Point(8, 247);
+            this.AccTypelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.AccTypelbl.Name = "AccTypelbl";
-            this.AccTypelbl.Size = new System.Drawing.Size(77, 13);
+            this.AccTypelbl.Size = new System.Drawing.Size(99, 17);
             this.AccTypelbl.TabIndex = 12;
             this.AccTypelbl.Text = "Account Type:";
             // 
@@ -168,35 +183,39 @@ namespace BankGG
             this.accTypeCB.Items.AddRange(new object[] {
             "Savings",
             "Current"});
-            this.accTypeCB.Location = new System.Drawing.Point(89, 198);
+            this.accTypeCB.Location = new System.Drawing.Point(119, 244);
+            this.accTypeCB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.accTypeCB.Name = "accTypeCB";
-            this.accTypeCB.Size = new System.Drawing.Size(128, 21);
+            this.accTypeCB.Size = new System.Drawing.Size(169, 24);
             this.accTypeCB.TabIndex = 13;
             // 
             // Amountlbl
             // 
             this.Amountlbl.AutoSize = true;
-            this.Amountlbl.Location = new System.Drawing.Point(37, 233);
+            this.Amountlbl.Location = new System.Drawing.Point(49, 287);
+            this.Amountlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Amountlbl.Name = "Amountlbl";
-            this.Amountlbl.Size = new System.Drawing.Size(46, 13);
+            this.Amountlbl.Size = new System.Drawing.Size(60, 17);
             this.Amountlbl.TabIndex = 14;
             this.Amountlbl.Text = "Amount:";
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(89, 230);
+            this.maskedTextBox1.Location = new System.Drawing.Point(119, 283);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.maskedTextBox1.Mask = "00000000";
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(128, 20);
+            this.maskedTextBox1.Size = new System.Drawing.Size(169, 22);
             this.maskedTextBox1.TabIndex = 15;
             this.maskedTextBox1.ValidatingType = typeof(int);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 262);
+            this.label1.Location = new System.Drawing.Point(37, 322);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 17;
             this.label1.Text = "Birth Date:";
             // 
@@ -206,9 +225,10 @@ namespace BankGG
             this.SubmitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SubmitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.SubmitBtn.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SubmitBtn.Location = new System.Drawing.Point(493, 141);
+            this.SubmitBtn.Location = new System.Drawing.Point(657, 174);
+            this.SubmitBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SubmitBtn.Name = "SubmitBtn";
-            this.SubmitBtn.Size = new System.Drawing.Size(130, 78);
+            this.SubmitBtn.Size = new System.Drawing.Size(173, 96);
             this.SubmitBtn.TabIndex = 18;
             this.SubmitBtn.Text = "Submit Details";
             this.SubmitBtn.UseVisualStyleBackColor = false;
@@ -220,9 +240,10 @@ namespace BankGG
             this.Cancelbtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Cancelbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Cancelbtn.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cancelbtn.Location = new System.Drawing.Point(493, 225);
+            this.Cancelbtn.Location = new System.Drawing.Point(657, 277);
+            this.Cancelbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Cancelbtn.Name = "Cancelbtn";
-            this.Cancelbtn.Size = new System.Drawing.Size(130, 78);
+            this.Cancelbtn.Size = new System.Drawing.Size(173, 96);
             this.Cancelbtn.TabIndex = 19;
             this.Cancelbtn.Text = "Cancel";
             this.Cancelbtn.UseVisualStyleBackColor = false;
@@ -230,15 +251,26 @@ namespace BankGG
             // 
             // Birthdate
             // 
-            this.Birthdate.Location = new System.Drawing.Point(89, 262);
+            this.Birthdate.Location = new System.Drawing.Point(119, 322);
+            this.Birthdate.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.Birthdate.Name = "Birthdate";
             this.Birthdate.TabIndex = 20;
             // 
-            // Form2
+            // guna2BorderlessForm1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.guna2BorderlessForm1.AnimateWindow = true;
+            this.guna2BorderlessForm1.AnimationInterval = 300;
+            this.guna2BorderlessForm1.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_CENTER;
+            this.guna2BorderlessForm1.BorderRadius = 40;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // AddAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.Birthdate);
             this.Controls.Add(this.Cancelbtn);
             this.Controls.Add(this.SubmitBtn);
@@ -259,7 +291,9 @@ namespace BankGG
             this.Controls.Add(this.label2);
             this.Controls.Add(this.FirstNtext);
             this.Controls.Add(this.lblName);
-            this.Name = "Form2";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AddAccount";
             this.Text = "Add Account";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
@@ -289,5 +323,6 @@ namespace BankGG
         private System.Windows.Forms.Button SubmitBtn;
         private System.Windows.Forms.Button Cancelbtn;
         private System.Windows.Forms.MonthCalendar Birthdate;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
     }
 }
--- BankGG-windowsApp/BankGG/Mainmenu.cs
+++ BankGG-windowsApp/BankGG/Mainmenu.cs
@@ -22,30 +22,8 @@ namespace BankGG
         }
 
         
-        private void addAccbtn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                AddAccount f = new AddAccount();
-                f.Show();
-            }
-            catch(Exception eg)
-            {
-                MessageBox.Show("Unable to create account"+eg);
-            }
-        }
-
-        private void fetchdetailsbtn_Click(object sender, EventArgs e)
-        {
-            DetailsForm f = new DetailsForm();
-            f.Show();
-        }
-
-        private void delAccbtn_Click(object sender, EventArgs e)
-        {
-            DeleteForm f = new DeleteForm();
-            f.Show();
-        }
+       
+  
 
         private void withbtn_Click(object sender, EventArgs e)
         {
@@ -57,6 +35,51 @@ namespace BankGG
         {
             Deposit d = new Deposit();
             d.Show();
+        }
+
+      
+
+        private void AddAccountBtn_Click_1(object sender, EventArgs e)
+        {
+
+            try
+            {
+                AddAccount f = new AddAccount();
+                f.Show();
+            }
+            catch (Exception eg)
+            {
+                MessageBox.Show("Unable to create account" + eg);
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            DeleteForm f = new DeleteForm();
+            f.Show();
+        }
+
+        private void ViewDetails_Click(object sender, EventArgs e)
+        {
+            DetailsForm f = new DetailsForm();
+            f.Show();
+        }
+
+        private void WithCashBtn_Click(object sender, EventArgs e)
+        {
+            Withdraw wd = new Withdraw();
+            wd.Show();
+        }
+
+        private void Depositbtn_Click(object sender, EventArgs e)
+        {
+            Deposit f = new Deposit();
+            f.Show();
+        }
+
+        private void FormCloseBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
